706d40a53b74b9ff0a18fbb77573b5ec1bb3639b
hasibul1670 <hasibulislam1670@gmail.com>
2024-08-30 22:37:36 +0600

    hello

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+// hello.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int x ;
+    cin>>x;
+    cout << "Hello World! \n";
+    cout << "I am learning C++" << endl<< x << endl;
+    return 0;
+}
