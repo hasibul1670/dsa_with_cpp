4b521353ac687ab3c895ff25978562e4a424b20c
hasibul1670 <hasibulislam1670@gmail.com>
2024-08-30 22:47:48 +0600

    sdsd

---
+++ aaa.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int x ;
+    cin>>x;
+    cout << "Hello World! \n";
+    cout << "I am learning C++" << endl<< x << endl;
+    return 0;
+}
