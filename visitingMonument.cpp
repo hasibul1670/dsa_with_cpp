d997d22134f116197f40f4f9715f48c4a9241495
Md. Hasibul Islam <hasibulislam1670@gmail.com>
2024-11-11 07:40:55 +0600

    learn :: Matrix Learn !!

--- visitingMonument.cpp
+++ visitingMonument.cpp
@@ -1,9 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+int num = 1;
+
+void visitingMonument(int n)
+{
+    if (n > 0) {
+        cout << n << " ";
+        visitingMonument(n - 1); 
+    }
+}
+void visitingMonument2(int n)
+{
+    if (n > 0) {
+        cout << n << " ";
+        visitingMonument2(n - 1); 
+    }
+}
+
 int main()
 {
-  int a;
-  cin >> a;
-  cout << "Hello hasib" << endl;
+  int t, x;
+  cin >> t;
+  while (t--) {
+        int n;
+        cin >> n;
+        visitingMonument(n);
+        visitingMonument2(n - 1);
+        cout << endl;
+    }
   return 0;
 }
